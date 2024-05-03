bbe34b18791e9f945eb3065d97cedce632f939c8
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-03 15:28:59 +0000

    Deploying to gh-pages from @ danretegan/interactive-ninja-quiz@47ed24e5e031ad34d5d43a76c9ad0a6f53d5b141 🚀

--- index.1fd90e97.js
+++
@@ -1,2 +0,0 @@
-var correctAnswers=["B","B","B","B"],form=document.querySelector(".quiz-form");form.addEventListener("submit",(function(e){e.preventDefault();var r=[form.q1.value,form.q2.value,form.q3.value,form.q4.value].reduce((function(e,r,o){return r===correctAnswers[o]?e+25:e}),0);console.log(r)}));
-//# sourceMappingURL=index.1fd90e97.js.map
